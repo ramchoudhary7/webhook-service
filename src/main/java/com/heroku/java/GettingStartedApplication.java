f5409ab28477d63559dabdf533196b808fecd379
ram.choudhary <ram.choudhary@flipkarthealthplus.com>
2023-09-05 00:16:39 +0530

    added health check endpoint

--- src/main/java/com/heroku/java/GettingStartedApplication.java
+++ src/main/java/com/heroku/java/GettingStartedApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -50,5 +51,14 @@ public class GettingStartedApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GettingStartedApplication.class, args);
+    }
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping("/health-check")
+    public String sayHello() {
+        return "Hello, World! Bring it on!!";
     }
 }
